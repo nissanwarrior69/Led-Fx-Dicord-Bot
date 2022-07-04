19a65c6a06b46aee82889fd4ff75ad09e50d9d90
nissanwarrior69 <dhjnadeshotjr@gmail.com>
2022-07-03 19:04:02 -0700

    init bot dev

---
+++ index.ts
@@ -0,0 +1,17 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Bot is ready')
+})
+
+client.login(process.env.TOKEN)
